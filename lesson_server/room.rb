1de54ab4bcd6125e61fe8bb64b18b2986791c8a5
Anton Mishchuk <anton.mishchuk@gmail.com>
2013-05-03 11:46:57 +0300

    add statistic

--- lesson_server/room.rb
+++ lesson_server/room.rb
@@ -10,7 +10,7 @@ class Room
     @editor_text = room['editor_text'] || DEFAULT_EDITOR_TEXT
     @chat_messages = room['chat_messages'] || []
     @channel = EM::Channel.new
-    @@rooms.merge!(@id => self)
+    self.class.rooms.merge!(@id => self)
   end
 
   def add_user(user_id, ws, info)
@@ -39,17 +39,15 @@ class Room
   end
 
 
-  @@rooms = {}
-
   class << self
 
     def rooms
-      @@rooms
+      @rooms ||= {}
     end
 
     def find_or_create_room(credentials, ws, info)
       room_id = credentials['room_id']
-      if room = @@rooms[room_id]
+      if room = rooms[room_id]
         user_id = credentials['user_id']
         room.add_user(user_id, ws, info)
       else
@@ -58,5 +56,19 @@ class Room
         room.add_user(user_id, ws, info)
       end
     end
+
+    def statistic
+      stats = '='*50 + "\n"
+      stats << "Rooms total: #{rooms.keys.count}\n"
+      rooms.each do |room_id, value|
+        stats << "#{room_id}: #{value.users.keys.join(', ')}\n"
+      end
+      stats << '='*50 + "\n"
+      stats
+    end
+  end
+
+  EM::PeriodicTimer.new(10) do
+    puts statistic
   end
 end
