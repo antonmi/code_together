e717fd4a0252444c9c7e738d04651c9e1cbf49ca
Anton Mishchuk <anton.mishchuk@gmail.com>
2013-05-25 13:12:27 +0300

    master for heroku

--- server/config.rb
+++ server/config.rb
@@ -7,6 +7,6 @@ require 'web_socket_server'
 
 DEFAULT_EDITOR_TEXT = 'Welcome!'
 EDITOR_TEXT_MAX_SIZE = 50000
-SERVER_HOST = '185.4.65.79'
+SERVER_HOST = 'thawing-garden-2267.herokuapp.com'#'185.4.65.79'
 SERVER_PORT = '9090'
 
